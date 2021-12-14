93c22e4dcd86370746c6d5909dd72e911a931f71
Michaël <bugnone.michael@gmail.com>
2021-12-14 01:30:05 +0100

    Socket.io

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
+//Clear and create
 // const db = require("./models");
 // db.sequelize.sync({ force: true }).then(() => {
 //     console.log("Drop and re-sync db.");
@@ -22,8 +23,28 @@ app.get("/", (req, res) => {
 require("./routes/user.routes")(app);
 require("./routes/login.routes")(app);
 
+// ajout de socket.io
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(8080, () => {
   console.log(`Server is running: http://127.0.0.1:${PORT}/`);
 });
+
+
+// var io = require('socket.io').listen(server);
+
+// io.sockets.on('connection', function(socket){
+//     socket.on('stream', function(image){
+//      socket.broadcast.emit('stream', image);
+//     });
+// });
