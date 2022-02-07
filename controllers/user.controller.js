a217a61ac61471580c51ae9a37edad5cacb68b56
Michaël BUGNONE <bugnone.michael@gmail.com>
2022-02-07 22:50:07 +0100

    Up

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -7,35 +7,34 @@ const saltRounds = 10;
 
 // Create a user
 exports.create = async (req, res) => {
-    const { email, password, role } = req.body;
+  const { email, password } = req.body;
 
-    // Validate user input
-    if (!(email && password && role)) {
-        return res.status(400).send("All input is required");
-    }
+  // Validate user input
+  if (!(email && password)) {
+    return res.status(400).send("All input is required");
+  }
 
-    //Check if user already exists
-    const checkUser = await User.findOne({
-        where: {
-            email: email,
-        },
-    });
+  //Check if user already exists
+  const checkUser = await User.findOne({
+    where: {
+      email: email,
+    },
+  });
 
-    if (checkUser) {
-        return res.status(409).send("User Already Exist.");
-    }
+  if (checkUser) {
+    return res.status(409).send("User Already Exist.");
+  }
 
-    bcrypt.hash(password, saltRounds)
-        .then(hash => {
-            const user = {
-                email: email,
-                password: hash,
-                role: role
-            };
+  bcrypt.hash(password, saltRounds)
+    .then(hash => {
+      const user = {
+        email: email,
+        password: hash,
+      };
 
-            User.create(user)
-                .then((value) => res.status(201).json({ value }))
-                .catch(error => res.status(400).json({ error }));
-        })
-        .catch(error => res.status(500).json({ error }));
+      User.create(user)
+        .then((value) => res.status(201).json({ value }))
+        .catch(error => res.status(400).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
 };
